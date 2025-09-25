133b731d7e6800886d59840afeb4666081712955
Tonnie-debug-svg <tonniekbett@gmail.com>
2025-09-26 01:24:28 +0300

    Completed domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+    .then((response) => response.json())
+    .then((object) => {
+      // append new id to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = `New user created with ID: ${object.id}`;
+      body.appendChild(p);
+      return object;
+    })
+    .catch((error) => {
+      // append error message to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      body.appendChild(p);
+    });
+}
+document.addEventListener("DOMContentLoaded", function() {
+  const paragraph = document.getElementById("text");
+  paragraph.textContent = "This is really cool!";
+});
